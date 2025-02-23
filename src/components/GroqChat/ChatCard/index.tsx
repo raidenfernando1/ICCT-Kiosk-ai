095890aed148b8a9944314d0136cbd3986ce3e44
Raiden Fernando <157260780+raidenfernando1@users.noreply.github.com>
2025-02-23 23:40:40 +0800

    Dev: Minor changes

--- src/components/GroqChat/ChatCard/index.tsx
+++ src/components/GroqChat/ChatCard/index.tsx
@@ -5,7 +5,7 @@ export const AssistantCard: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <div className="AssistantCard">
+    <div className={styles.AssistantCard}>
       <li>{children}</li>
     </div>
   );
@@ -15,8 +15,9 @@ export const UserCard: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <div className="UserCard">
+    <div className={styles.UserCard}>
       <li>{children}</li>
     </div>
   );
 };
+  
