05dd8f304e226d664b82d2f6fab50a893b99e635
Raiden Fernando <157260780+raidenfernando1@users.noreply.github.com>
2025-02-25 03:05:45 +0800

    Dev: Added MarkDown support + added styles for better formatting

--- src/components/GroqChat/ChatCard/index.tsx
+++ src/components/GroqChat/ChatCard/index.tsx
@@ -1,23 +1,19 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
 import styles from "./styles.module.css";
 
-export const AssistantCard: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+export const AssistantCard: React.FC<{ children: string }> = ({ children }) => {
   return (
     <div className={styles.AssistantCard}>
-      <li>{children}</li>
+      <ReactMarkdown>{children}</ReactMarkdown>
     </div>
   );
 };
 
-export const UserCard: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+export const UserCard: React.FC<{ children: string }> = ({ children }) => {
   return (
     <div className={styles.UserCard}>
-      <li>{children}</li>
+      <ReactMarkdown>{children}</ReactMarkdown>
     </div>
   );
 };
-  
