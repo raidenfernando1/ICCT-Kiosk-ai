06fc3943d37f4953af4ceb6a0416aefc82fcfe0d
Raiden Fernando <157260780+raidenfernando1@users.noreply.github.com>
2025-03-02 01:39:46 +0800

    removed unecessary test insert

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,19 +1,3 @@
-import { insertData } from "../../hooks/useSupabase";
-
 export const AdminPage = () => {
-  return (
-    <div>
-      <button
-        onClick={() =>
-          insertData(
-            "other",
-            "who where your creators or developers",
-            `my creator is Raiden Fernando`
-          )
-        }
-      >
-        test
-      </button>
-    </div>
-  );
+  return <div></div>;
 };
